2603cd3228abd15cbf0df66ef7b15f650f92b2be
lburgwardt <loloburgwardt@burgwardt.com.ar>
2020-05-25 15:48:57 -0300

    agregamos estados

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,8 +1,14 @@
+const fs =require('fs')
 const bcryptjs = require('bcryptjs');
 const controller = {
     login: function(req, res){
-        if(bcryptjs.compareSync(req.body.password, req.user.password)){
-            req.session.user = req.user
+        let users = fs.readFileSync(__dirname + '/../data/users.json', 'utf-8');
+        users = JSON.parse(users);
+        let user = users.find(function (value) {
+            return req.body.email == value.email
+        })
+        if(bcryptjs.compareSync(req.body.password, user.password)){
+            req.session.user = user;
             res.redirect('/registrar')
         }else{
             res.redirect('/')
